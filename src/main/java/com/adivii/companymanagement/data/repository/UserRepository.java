448a9ac1312431b3fcaac0e537291fdce767bbc8
Adiwijaya Satria Nusantara <adihanyu@gmail.com>
2023-03-14 10:45:20 +0700

    update UserRepository

--- src/main/java/com/adivii/companymanagement/data/repository/UserRepository.java
+++ src/main/java/com/adivii/companymanagement/data/repository/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.adivii.companymanagement.data.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    public List<User> findByEmailAddress(String email);
+    public List<User> findByEmail(String email);
 }
